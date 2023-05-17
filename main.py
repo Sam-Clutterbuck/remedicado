43dff2d74061efd947755a872c2a1335c22532af
Sam-Clutterbuck <SamuelClutterbuck@outlook.com>
2023-05-17 17:44:47 +0100

    fixed main

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
 from src import Cli
 
-test ={"TEST":1,"test 2": 2}
-
-for row in test:
-
-    print(test[row])
-
-#Cli.Cli_Start()
-
-#'data/files/Tenable_Remediations_Breakdown.csv','test_data/DEMO_TOP_10.csv', 'test_data/DEMO_UPLOAD.csv'
+Cli.Cli_Start()
